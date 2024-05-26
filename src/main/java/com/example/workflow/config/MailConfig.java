3a4bec9e458e138f06c72267075c52b978c4bc60
thiago daudt fcamara <thiago.daudt@fcamara.com>
2024-05-26 16:10:41 -0300

    Send email correctly

--- src/main/java/com/example/workflow/config/MailConfig.java
+++ src/main/java/com/example/workflow/config/MailConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.example.workflow.config;
 
 import org.springframework.context.annotation.Bean;
@@ -5,12 +6,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Properties;
+
 @Configuration
 public class MailConfig {
 
     @Bean
     public JavaMailSender getJavaMailSender() {
-        return new JavaMailSenderImpl();
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("thiagobarrocal@gmail.com");
+        mailSender.setPassword("ostrafrita");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
     }
 
 }
+*/
--- src/main/java/com/example/workflow/service/email/EmailSender.java
+++ src/main/java/com/example/workflow/service/email/EmailSender.java
@@ -1,25 +1,43 @@
 package com.example.workflow.service.email;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@Service
-public class EmailSender {
-    private JavaMailSender mailSender;
+import java.io.IOException;
 
-    @Autowired
-    public EmailSender(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
+@Service
+@Slf4j
+public class EmailSender {
+
+    @Value("${SENDGRID_API_KEY}")
+    public String SENDGRID_API_KEY;
 
     public void sendSimpleMessage(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("noreply@baeldung.com");
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        mailSender.send(message);
+
+        Email emailFrom = new Email("thiagobarrocal@gmail.com");
+        Email emailTo = new Email(to);
+        Content content = new Content("text/plain", text);
+        Mail mail = new Mail(emailFrom, subject, emailTo, content);
+
+        SendGrid sg = new SendGrid(SENDGRID_API_KEY);
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            log.info("Email sent successfully to '{}' with subject '{}'", to, subject);
+
+        } catch (IOException ex) {
+            log.error("Error sending email to '{}'", to, ex);
+        }
     }
 }
